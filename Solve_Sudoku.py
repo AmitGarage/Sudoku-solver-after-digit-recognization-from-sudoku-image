ffe528543c3fc12045e19e167b622a5a618bceff
Amit Singh <amitsingh9994@gmail.com>
2021-05-04 16:07:29 +0530

    Update Solve_Sudoku.py

--- Solve_Sudoku.py
+++ Solve_Sudoku.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,9 +18,6 @@ from skimage import measure,feature
 import skimage.transform as tnf
 from skimage.morphology import erosion,dilation,closing,square,disk,binary_dilation,binary_erosion
 from skimage.segmentation import clear_border
-
-
-# In[2]:
 
 
 class cnn_model(nn.Module) :
@@ -44,111 +38,74 @@ class cnn_model(nn.Module) :
         input_x = self.fc2(input_x)
         return F.log_softmax(input_x)
 
-
-# In[3]:
-
-
-#learning_rate = 0.01
-learning_rate = 0.001
-momentum = 0.6
-#no_of_epochs = 80
-no_of_epochs = 20
-
 device_to_use = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-#Model = cnn_model()
-#Model.to(device_to_use)
-#optimizer = optim.Adam(Model.parameters(),lr=learning_rate,betas=(0.9,0.999))#,momentum=momentum)
-
-
-# In[4]:
-
-
+# Opening image from which sudoku needs to extract
 sudoku_image = Image.open('/storage/Suduko_solver_data/Suduko120.jpg')
+# Converting image to grayscale
 sudoku_image = sudoku_image.convert('L')
+# Saving image in form of array
 sukodu_image_array = np.array(sudoku_image)
 #sukodu_image_array = sukodu_image_array[10:-10,10:-10]
 #sukodu_image_array[sukodu_image_array > 100] = 255
+# Displaying image
 plt.figure()
 plt.imshow(sukodu_image_array, cmap='gray')
 
+#print(sukodu_image_array.shape)
 
-# In[5]:
-
-
-print(sukodu_image_array.shape)
-
-
-# In[6]:
-
-
+# Computing a threshold mask image based on local pixel neighborhood
 threshold = threshold_local(sukodu_image_array,block_size=47,offset=20)
+# Extracting image above threshold value
 filtered_image = sukodu_image_array > threshold
 plt.figure()
 plt.imshow(filtered_image, cmap='gray')
 
-
-# In[7]:
-
-
+# Extracting contours from image
 contours = measure.find_contours(filtered_image)
 #print(len(contours))
 #print(contours[720])
 #print(contours[720][:,1])
 #print(contours[720][:,0])
 
+#fig, ax = plt.subplots()
+#ax.imshow(filtered_image, cmap=plt.cm.gray)
 
-# In[8]:
+#for contour in contours[10:1000]:
+#    ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
 
-
-fig, ax = plt.subplots()
-ax.imshow(filtered_image, cmap=plt.cm.gray)
-
-for contour in contours[10:1000]:
-    ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-
-ax.axis('image')
-ax.set_xticks([])
-ax.set_yticks([])
-plt.show()
-
-
-# In[9]:
-
+#ax.axis('image')
+#ax.set_xticks([])
+#ax.set_yticks([])
+#plt.show()
 
 contour_list = []
 contour_number = 0
+# Creating contour list to extract contour with maximum area
 for n , contour in enumerate(contours) :
     contour_details = []
     contour_details.append(len(contour))
     contour_details.append(contour_number)
     contour_list.append(contour_details)
     contour_number += 1
+    
+# Sorting contour list in reverse order
 contour_list.sort(key=lambda x:x[0] ,reverse = True)
 contour_list = np.array(contour_list)
 #print(contour_list[:,1])
 contour_to_traverse = []
+# Traversing contour list and extracting coordinates
 for i in contour_list[:,1] :
     contour_to_traverse.append(contours[i])
 
-
-# In[10]:
-
-
 #print(contours[100:728],contour_to_traverse[0])
 
-
-# In[11]:
-
-
+# Function to calculate area proportion based on original area and cropped area
 def calculate_area_proportion ( original_height , original_width ,height , width ) :
     cropped_area = height * width
     original_area = original_height * original_width
     area_proportion = (cropped_area/original_area)*100
     return area_proportion
-
-
-# In[12]:
 
 
 fig , axis = plt.subplots()
@@ -159,10 +116,13 @@ appproximate_sudoku = []
 c1,c2,c3,c4 = np.array([]),np.array([]),np.array([]),np.array([])
 #final_coordinates = np.array([])
 for contour_cordinates in contour_to_traverse :
+    # Only traverse when 1st coordinates matches with last coordinates and no of coordinates in that contour is greater than 3
     if contour_cordinates[0,0] == contour_cordinates[len(contour_cordinates)-1,0] and contour_cordinates[0,1] == contour_cordinates[len(contour_cordinates)-1,1] and len(contour_cordinates) > 3:
         new_sudoku = contour_cordinates.copy()
+        # Approximate a polygonal with the specified tolerance.
         appproximate_sudoku = measure.approximate_polygon(new_sudoku,tolerance=20.0)
         #print(min(appproximate_sudoku[:,0]),max(appproximate_sudoku[:,0]),min(appproximate_sudoku[:,1]),max(appproximate_sudoku[:,1]))
+        # Traversing sudoku and comparing polygon coordinates with min/max of polygon coordinates - orginal height / weight with a margin of 6% and find all 4 coordinates
         for row in appproximate_sudoku :
             #print(row[0],row[1])
             #print(min(appproximate_sudoku[:,0])-(original_height*0.06),min(appproximate_sudoku[:,0])+(original_height*0.06),min(appproximate_sudoku[:,1])-(original_width*0.06),min(appproximate_sudoku[:,1])+(original_width*0.06))
@@ -183,6 +143,7 @@ for contour_cordinates in contour_to_traverse :
                 #print('c4')
         #print(np.array([min(appproximate_sudoku[:,0]),min(appproximate_sudoku[:,1])]))
         #print(len(c2))
+        # If any of coordinates not found , then will be used as below
         if len(c1) == 0 :
             c1 = np.array([min(appproximate_sudoku[:,0]),min(appproximate_sudoku[:,1])])
         if len(c2) == 0 :
@@ -206,13 +167,17 @@ for contour_cordinates in contour_to_traverse :
 #for i in range(len(appproximate_sudoku)):
 #    axis.scatter(appproximate_sudoku[i][1],appproximate_sudoku[i][0])
 
+# Arranging sudoku coordinates in order either in clockwise / Anit-clockwise
+# Flip array(entries in each column) in left-right direction
 desired_matrix = np.fliplr(appproximate_sudoku[0:4])
 #print(desired_matrix)
+# Sorting along y axis
 desired_matrix = desired_matrix[desired_matrix[:,1].argsort()]
 #print(desired_matrix)
 desired_matrix1 = desired_matrix[:2]
 desired_matrix2 = desired_matrix[2:]
 #print(desired_matrix1,desired_matrix2)
+# Sorting along x axis
 desired_matrix1 = desired_matrix1[desired_matrix1[:,0].argsort()]
 desired_matrix2 = desired_matrix2[desired_matrix2[:,0].argsort()[::-1]]
 #print(desired_matrix1,desired_matrix2)
@@ -222,9 +187,12 @@ desired_matrix = np.concatenate((desired_matrix1,desired_matrix2),axis=0)
 #print(np.sqrt(np.sum((desired_matrix[1]-desired_matrix[2])**2)))
 #print(np.sqrt(np.sum((desired_matrix[2]-desired_matrix[3])**2)))
 #print(np.sqrt(np.sum((desired_matrix[3]-desired_matrix[0])**2)))
+# Calculating height and width from extracted sudoku coordinates
 cropped_height = np.sqrt(np.sum((desired_matrix[0]-desired_matrix[1])**2))
 cropped_width = np.sqrt(np.sum((desired_matrix[2]-desired_matrix[3])**2))
 #print(calculate_area_proportion(original_height , original_width ,cropped_height , cropped_width))
+# Calculating area percentage of extracted sudoku over orignal image
+# If less than 20% , then whole image is sudoku and setting coordinates again
 if calculate_area_proportion(original_height , original_width ,cropped_height , cropped_width) < 20 :
     c1 = np.array([0,0])
     c2 = np.array([original_height,0])
@@ -239,29 +207,25 @@ if calculate_area_proportion(original_height , original_width ,cropped_height , 
     axis.plot(appproximate_sudoku[:, 1], appproximate_sudoku[:, 0], linewidth=2)
     desired_matrix = appproximate_sudoku[0:4]
     #print(desired_matrix)
-    
-for i in range(len(appproximate_sudoku)):
-    axis.scatter(appproximate_sudoku[i][1],appproximate_sudoku[i][0])
 
+#for i in range(len(appproximate_sudoku)):
+#    axis.scatter(appproximate_sudoku[i][1],appproximate_sudoku[i][0])
 
-# In[13]:
-
-
+# Initalizing final sudoku size over which extracted image will be transformed
 final_sudoku_size = np.array(((0,0),(270,0),(270,270),(0,270)))
 transformation = tnf.ProjectiveTransform()
 transformation_estimation = transformation.estimate(final_sudoku_size,desired_matrix)
 #print(transformation_estimation,transformation)
+# Warp an image according to a given coordinate transformation
 Wrapping = tnf.warp(filtered_image,transformation,output_shape=(270,270))
 #print(Wrapping,np.amax(Wrapping))
+# Inverting images values to convert black to white and vice versa
 Wrapping = abs(Wrapping-np.amax(Wrapping))
 #print(Wrapping,np.amax(Wrapping))
 plt.figure()
 plt.imshow(Wrapping, cmap='gray')
 
-
-# In[14]:
-
-
+# Removing borders ( value of all pixels on most of column or row is 1 / 0 ) if any on all the 4 sides of image
 #print(Wrapping[0,:],Wrapping[-1,:],Wrapping[:,0],Wrapping[:,-1],len(Wrapping[0,:]))
 reduce_x_0 , reduce_x_1 , reduce_y_0 , reduce_y_1 = 0,0,0,0
 for x in range(int(len(Wrapping[0,:])/2)) :
@@ -281,7 +245,7 @@ for x in range(int(len(Wrapping[0,:])/2)) :
         print('reduce_y_1 - '+str(reduce_y_1))
     if ( np.count_nonzero(Wrapping[x,:]==1) < len(Wrapping[0,:])-5 or np.count_nonzero(Wrapping[x,:]==0) > len(Wrapping[0,:])-5 ) and ( np.count_nonzero(Wrapping[-x-1,:]==1) < len(Wrapping[0:])-5 or np.count_nonzero(Wrapping[-x-1,:]==0) > len(Wrapping[0:])-5 )and ( np.count_nonzero(Wrapping[:,x]==1) < len(Wrapping[:,0])-5 or np.count_nonzero(Wrapping[:,x]==0) > len(Wrapping[:,0])-5 ) and ( np.count_nonzero(Wrapping[:-x-1]==1) < len(Wrapping[:,0])-5 or np.count_nonzero(Wrapping[:-x-1]==0) > len(Wrapping[:,0])-5 ) :
         print('break')
-        #print(reduce_x_0,reduce_x_1,reduce_y_0,reduce_y_1)
+        print(reduce_x_0,reduce_x_1,reduce_y_0,reduce_y_1)
         if reduce_x_0 != 0 or reduce_x_1 != 0 or reduce_y_0 != 0 or reduce_y_1 != 0:
             if reduce_x_1 == 0 :
                 reduce_x_1 = 1
@@ -297,55 +261,39 @@ for x in range(int(len(Wrapping[0,:])/2)) :
             final_coordinates = np.array([c1,c2,c3,c4,c5])
             appproximate_sudoku = final_coordinates
             desired_matrix = appproximate_sudoku[0:4]
+            # Image size reduced because of border removal . So , again transformation
             final_sudoku_size = np.array(((0,0),(270,0),(270,270),(0,270)))
             transformation = tnf.ProjectiveTransform()
             transformation_estimation = transformation.estimate(final_sudoku_size,desired_matrix)
-            #print(transformation_estimation,transformation)
+            print(transformation_estimation,transformation)
             Wrapping = tnf.warp(Wrapping,transformation,output_shape=(270,270))
         break;
 plt.figure()
 #print(reduce)
 plt.imshow(Wrapping, cmap='gray')
 
-
-# In[15]:
-
-
 selem = square(1)
 #print(selem)
 #print(closing(Wrapping,selem))
 plt.figure()
 plt.imshow(closing(Wrapping,selem), cmap='gray')
+#Closing remove small dark spots and connect small bright cracks
 final_wrapped_image = closing(Wrapping,selem)
 
-
-# In[16]:
-
-
-plt.figure()
-plt.imshow(binary_dilation(Wrapping,selem), cmap='gray')
+#plt.figure()
+#plt.imshow(binary_dilation(Wrapping,selem), cmap='gray')
 #final_wrapped_image = binary_dilation(Wrapping,selem)
 
-
-# In[17]:
-
-
 #selem=disk(2)
-plt.figure()
-plt.imshow(closing(erosion(Wrapping,selem),square(4)), cmap='gray')
+#plt.figure()
+#plt.imshow(closing(erosion(Wrapping,selem),square(4)), cmap='gray')
 #plt.imshow(dilation(Wrapping,selem), cmap='gray')
 #final_wrapped_image = erosion(Wrapping,selem)
 
-
-# In[18]:
-
-
+# Initalizing 9 x 9 sudoku array with 0
 Sudoku = np.zeros([9,9],dtype=int)
 
-
-# In[19]:
-
-
+# Function for removing boundary
 def boundary_removal( input_image ) :
     height,width = input_image.shape
     input_image[:3,:] = 0
@@ -354,32 +302,27 @@ def boundary_removal( input_image ) :
     input_image[:,width-3:] = 0
     return input_image
 
-
-# In[20]:
-
-
-#plt.imshow(cell_dimensions, cmap='gray')
-
-
-# In[21]:
-
-
+# Function to extract digit from sudoku
 def digit_encoder() :
+    # Loading pretrained weights
     pretrained_weights = torch.load('/storage/Suduko_solver_data/model_2.pth')
     pretrained_model = cnn_model()
     pretrained_model.load_state_dict(pretrained_weights)
     pretrained_model.eval()
-    height , weight = final_wrapped_image.shape
-    print(height , weight)
+    height , width = final_wrapped_image.shape
+    #print(height , width)
     for x in range(9) :
         for y in range(9) :
-            #print((x*int(height/9))+1,((x+1)*int(height/9))-1,(y*int(weight/9))+1 ,((y+1)*int(weight/9))-1)
-            cell_dimensions = final_wrapped_image[(x*int(height/9))+1 : ((x+1)*int(height/9))-1,(y*int(weight/9))+1 : ((y+1)*int(weight/9))-1]
-            #plt.figure()
-            #plt.imshow(cell_dimensions, cmap='gray')
+            #print((x*int(height/9))+1,((x+1)*int(height/9))-1,(y*int(width/9))+1 ,((y+1)*int(width/9))-1)
+            # Cutting 30 x 30 image from sudoku for digit prediction
+            cell_dimensions = final_wrapped_image[(x*int(height/9))+1 : ((x+1)*int(height/9))-1,(y*int(width/9))+1 : ((y+1)*int(width/9))-1]
+            plt.figure()
+            plt.imshow(cell_dimensions, cmap='gray')
+            # Removing boundary
             cell_dimensions = boundary_removal(cell_dimensions)
             plt.figure()
             plt.imshow(cell_dimensions, cmap='gray')
+            # To check no of values are 0
             Zero_digit_check = np.count_nonzero(cell_dimensions==0)
             #print(Zero_digit_check)
             cell_dimensions=np.expand_dims(np.expand_dims(cell_dimensions, axis=0), axis=0)
@@ -387,14 +330,16 @@ def digit_encoder() :
             #print(cell_dimensions.shape)
             #print(cell_dimensions)
             #test_dataset= torchvision.datasets.Image(cell_dimensions,transform=Image_transformation_function)
+            # Converting to tensor for loading to device for prediction
             test_dataset = torch.from_numpy(cell_dimensions) #Image_transformation_function(cell_dimensions)
             test_dataset.to(device_to_use)
             #training_data_loader = torch.utils.data.DataLoader(MNIST_dataset_training,batch_size=64,shuffle=True)
             predicted_output = pretrained_model(test_dataset.float())
-            #print(predicted_output)
+            print(predicted_output)
             predicted_digit = predicted_output.data.max(1,keepdim=True)[1]
             plt.title('Expected Digit - '+str(predicted_digit))
             #print(predicted_digit,predicted_output)
+            # If most of values are 0 , then can be considered as 0 values
             if predicted_digit == 1 and Zero_digit_check > 760 :
                 Sudoku[x][y] = 0
             else :
@@ -402,21 +347,12 @@ def digit_encoder() :
             #print(predicted_digit)
 
 
-# In[22]:
-
-
 digit_encoder()
-
-
-# In[23]:
-
 
 Sudoku
 
 
-# In[24]:
-
-
+# Function to check zero place in sudoku
 def zero_check( Sudoku ) :
     height , width = Sudoku.shape
     for i in range(height) :
@@ -426,10 +362,12 @@ def zero_check( Sudoku ) :
                 return (i,j)
     return None
 
+#zero_check( Sudoku ) 
 
-# In[25]:
+#print(Sudoku[:,0])
+#np.any(Sudoku[0]==7)
 
-
+# Function to backtrack to solve sudoku
 def backtracking(Sudoku , expected_digit , position) :
     x , y = position
     height , width = Sudoku.shape
@@ -452,10 +390,7 @@ def backtracking(Sudoku , expected_digit , position) :
                 return False
     return True
 
-
-# In[26]:
-
-
+# Function to solve sudoku
 def Sudoku_solution( Sudoku ) :
     empty_cell = zero_check( Sudoku )
     if not empty_cell :
@@ -472,10 +407,5 @@ def Sudoku_solution( Sudoku ) :
             Sudoku[x][y] = 0
     return False
 
-
-# In[27]:
-
-
 Sudoku_solution( Sudoku )
 print(Sudoku)
-
